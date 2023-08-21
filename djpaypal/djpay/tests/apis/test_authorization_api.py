a9dc037b3249a89d040b8297b101621d6fcab89c
brahim024 <ifninos168@gmail.com>
2023-08-21 11:30:17 +0000

    change tets clinet

--- djpaypal/djpay/tests/apis/test_authorization_api.py
+++ djpaypal/djpay/tests/apis/test_authorization_api.py
@@ -1,45 +1,35 @@
 import pytest
 from django.urls import reverse
-from django.test import Client
 from django.contrib.auth.models import User
 from rest_framework.test import force_authenticate
 from djpaypal.djpay.views import GenerateTokenViewSet
 from djpaypal.djpay.models import PaypalInfo
+from django.test import Client
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_list_access_api_without_authentication():
     """
         tests that test authentication endpont without pass authentication
     """
-    client = Client()
+    client = Client
     client_url = reverse("djpay:token-list")
     response = client.get(path=client_url)
     assert response.status_code == 401
 
 
-@pytest.mark.skip(reason="Because I cant find djpay:token-get url")
+# @pytest.mark.skip(reason="Because I cant find djpay:token-get url")
 @pytest.mark.django_db
-def test_generate_access_token_list(user_factory, factory, paypal_token):
-    request = factory.get(reverse('djpay:token-list'))
-    force_authenticate(request, user=user_factory.create())
-    view = GenerateTokenViewSet.as_view({'get':'list'})
-    response = view(request)
+def test_generate_access_token_list(user_factory, paypal_token):
+    client = Client()
+    res = client.get(reverse("djpay:token-list")
+    user=user_factory.create()
+    client.login(username=user.username,password=user.password)
+    # view = GenerateTokenViewSet.as_view({'get':'list'})
+    # response = view(request)
 
-    assert response.status_code == 200
+    assert res.status_code == 200
     # assert response.data["client_id"] == paypal_token.client_id
 
-# @pytest.mark.skip
-@pytest.mark.django_db
-def test_generate_access_token_without_auth(user_factory, factory):
-    """
-        test GenerateTokenViewSet wihout login
-    """
-    request = factory.get(reverse("djpay:token-list"))
-    user = User.objects.create(username='test',password='test')
-
-    view = GenerateTokenViewSet.as_view({'get':'list'})
-    response = view(request)
-    assert response.status_code == 401
 
 # need to fix
 @pytest.mark.skip
--- djpaypal/djpay/tests/conftest.py
+++ djpaypal/djpay/tests/conftest.py
@@ -3,8 +3,8 @@ import pytest
 from djpaypal.djpay.models import PaypalToken
 from django.conf import settings
 from django.test import Client
-from rest_framework.test import APIRequestFactory
-
+# from rest_framework.test import APIRequestFactory
+from rest_framework.test import APIClient
 from djpaypal.djpay.views import GenerateTokenViewSet
 from unittest.mock import Mock
 from pytest_factoryboy import register
@@ -30,8 +30,8 @@ def paypal_token():
     return PaypalTokenFactory.create()
 
 @pytest.fixture
-def factory():
-    return APIRequestFactory()
+def api_factory():
+    return APIClient()
 
 
 @pytest.fixture
