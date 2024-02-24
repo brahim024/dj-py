29b8d424d87a0727b0b3a4c3add62327963c22ec
boughanm6@gmail.com <macbookpro@MacBook-Pro-de-macbook.local>
2024-02-24 15:55:26 +0100

    fix running tests for live mode

--- djpay/api/authorize/path.py
+++ djpay/api/authorize/path.py
@@ -9,13 +9,15 @@ class PayPalUrls:
     BASE_URL = "https://api-m.sandbox.paypal.com"
 
     @classmethod
-    def base_url(cls):
+    def base_url(self):
         """
         Get the base URL for PayPal API based on the current mode.
         """
         
-        cls.BASE_URL = "https://api.paypal.com" if settings.LIVE_MODE else cls.BASE_URL
-        return cls.BASE_URL
+        if settings.LIVE_MODE == True:
+            return "https://api.paypal.com"
+        else:
+            return self.BASE_URL
 
     
 
